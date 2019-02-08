7ed75277be735016c6c2a4ec126dcba97ba48a1b
casge <39999551+mel225@users.noreply.github.com>
2019-02-09 04:26:10 +0900

    Update testBL.js

--- testBL.js
+++ testBL.js
@@ -1,7 +1,7 @@
 javascript:
 (function(d, s){
   s = d.createElement("script");
-  s.src = "https://mel225.github.io/OngekiFunctinos/score2img.js";
+  s.src = "https://mel225.github.io/OngekiFunctions/score2img.js";
   d.head.appendChild(s);
   s.onload = function(){
     score2img();

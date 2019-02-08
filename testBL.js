ed8a78949de53b20ac65115b743035fbe18ce112
mel225 <mini20cat_drumer@outlook.jp>
2019-02-09 05:59:14 +0900

    [bugfix]セミコロン抜けてたので修整

--- testBL.js
+++ testBL.js
@@ -1,7 +1,7 @@
 javascript:
 (function(d,s){
   s = d.head.appendChild(d.createElement('script'));
-  s.src = "https://mel225.github.io/OngekiFunctions/score2img.js"
+  s.src = "https://mel225.github.io/OngekiFunctions/score2img.js";
   s.onload = function(){
     read_html2canvas().then(score2img);
   };

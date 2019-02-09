983f0e897397e96585ed6785f85b77891600655b
mel225 <mini20cat_drumer@outlook.jp>
2019-02-09 17:18:37 +0900

    [edit] ダウンロード形式は無理があったみたいなので、やっぱりimg以外の全要素の右クリック禁止にしてみる。

--- score2img.js
+++ score2img.js
@@ -65,7 +65,7 @@ function score2img(){
 
     document.body.oncontextmenu = "";
     document.oncontextmenu = "";
-    recursion(document.body.chileNodes);
+    recursion(document.body.children);
   });
 }
 

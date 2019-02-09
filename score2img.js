f855e5f85826d18bb3632bce4e33f212b5317803
mel225 <mini20cat_drumer@outlook.jp>
2019-02-09 14:53:02 +0900

    [bugfix] 対象のimg以外すべてに右クリック禁止をかけてみる

--- score2img.js
+++ score2img.js
@@ -84,7 +84,7 @@ function score2img(){
   document.oncontextmenu = "";
   document.body.childNodes.forEach(function(element){
     console.log(element);
-    if(!element.id || element.id.includes("img_")){
+    if(!element.id || !element.id.includes("img_")){
       element.oncontextmenu = function(){return false;};
     }
   });
